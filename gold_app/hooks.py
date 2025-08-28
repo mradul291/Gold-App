f7e82213640874065ddd1902e26016f33f537a30
mradul291 <mishramradul29@gmail.com>
2025-08-28 15:20:27 +0530

    Staff Pickup Workspace

--- gold_app/hooks.py
+++ gold_app/hooks.py
@@ -18,8 +18,10 @@ doctype_js = {
     "Sales Order": "public/js/sales_order_item.js",
     "Purchase Order": "public/js/purchase_order_item.js",
     "Stock Entry": "public/js/stock_entry.js",
-    "Purchase Receipt": "public/js/purchase_receipt.js"
+    "Purchase Receipt": "public/js/purchase_receipt.js",
 }
+
+doctype_list_js = {"Item Pickup": "public/js/item_pickup_list.js"}
 
 doc_events = {
     "Item": {
@@ -92,7 +94,6 @@ doc_events = {
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
